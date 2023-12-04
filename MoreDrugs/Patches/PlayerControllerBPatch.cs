0ce4e59d2abbeafc7fb784dcb9f261a26bf1e8bf
TompyxP <cntompkins245@gmail.com>
2023-12-03 20:10:18 -0700

    messin around

--- MoreDrugs/Patches/PlayerControllerBPatch.cs
+++ MoreDrugs/Patches/PlayerControllerBPatch.cs
@@ -21,6 +21,19 @@ namespace MoreDrugs.Patches
                 Traverse.Create(__instance).Field("jumpForce").SetValue(10f);
                 Traverse.Create(__instance).Field("climbSpeed").SetValue(7f);
             }
+            else
+            {
+                if (__instance.currentlyHeldObject.itemProperties.itemId == 0123456789)
+                {
+                    bool isSprinting = Traverse.Create(__instance).Field("isSprinting").GetValue<bool>();
+                    __instance.health = 100;
+                    __instance.jumpForce = 30;
+                    if (isSprinting)
+                    {
+                        Traverse.Create(__instance).Field("sprintMultiplier").SetValue(30f);
+                    }
+                }
+            }
         }
     }
 }
